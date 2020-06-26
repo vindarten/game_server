65b3e5e3c42049a42329be17ddf6f71df5ca886b
Alexey Galkov <galkov1996alex@mail.ru>
2020-06-26 11:44:55 +0300

    Version 1.3 Bug fixes and other minor improvements

--- robot.cpp
+++ robot.cpp
@@ -10,29 +10,28 @@
 
 const int MaxSize = 4096;
 const int NameSize = 20;
-const int AnsSize = 20;
+const int OptSize = 20;
 const int BuyRaw = 2;
 const int IPAddress = 1;
 const int PortNum = 2;
 const int RobotName = 3;
 const int Action = 4;
-const int NumPlayers = 5;
-const int NumGame = 5;
+const int NumPlayersOrNumGame = 5;
+#ifdef MYSERVER
+	const int NumArg = 3;
+#else
+	const int NumArg = 6;
+#endif
 
 class Socket {
 	int fd, BufSize;
 	char buf[MaxSize+1];
-	void add(char *AddBuf, int AddSize)
+	int SearchEndLine()
 	{
-		int i;
-		if (BufSize + AddSize > MaxSize) {
-			throw "Buffer is full";
-		}
-		for(i = 0; i < AddSize; i++) {
-			buf[BufSize + i] = AddBuf[i];
-		}
-		BufSize += AddSize;
-		buf[BufSize] = 0;
+		for(int i = 0; i < BufSize; i++)
+			if (buf[i] == '\n')
+				return i;
+		return -1;
 	}
 public:
 	Socket()
@@ -49,102 +48,55 @@ public:
 		write(fd, mes, strlen(mes));
 		printf("%s", mes);
 	}
-	void init(int argc, char **argv);
-	void GetStr(char *HelpBuf);
-	void StartGame(char **argv);
-};
-
-void Socket::init(int argc, char **argv)
-{
-	struct sockaddr_in addr;
-	if (argc < 6) {
-		throw "Not enough arguments";
-	}	
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		throw "socket returned -1";
-	}
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(strtol(argv[PortNum], NULL, 10));
-	if (!inet_aton(argv[IPAddress], &(addr.sin_addr))) {
-		throw "inet_aton returned 0";
-	}
-	if (connect(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
-		throw "connect returned -1";
-	}
-}
-	
-void Socket::GetStr(char *HelpBuf)
-{
-	char AddBuf[MaxSize];
-	int AddSize, i, n;
-	while (1) {
-		n = -1;
-		for(i = 0; i < BufSize; i++) {
-			if (buf[i] == '\n') {
-				n = i;
-				break;
-			}
-		}
-		if (n != -1 && BufSize > 0) {
-			for(i = 0; i < n; i++) {
-				HelpBuf[i] = buf[i];
-			}
-			HelpBuf[n] = 0;
-			for(i = 0; i < BufSize - n; i++) {
-				buf[i] = buf[i + n + 1];
-			}
-			BufSize -= n + 1;
-			#ifdef DEBUGGING
-				printf("//%s\n", HelpBuf);
-			#endif
-			return;
-		} else {			
-			AddSize = read(fd, AddBuf, MaxSize);
-			if (AddSize == 0) {
-				throw "The connection with server is lost";
-			}
-			add(AddBuf, AddSize);
-		}
-	}
-}  
-#ifndef MYSERVER
-	void Socket::StartGame(char **argv)
+	void init(int argc, char **argv)
 	{
-		char HelpBuf[MaxSize];
-		char answer[AnsSize] = "";
-		int i = 0, n;
-		sprintf(HelpBuf, "%s\n", argv[RobotName]);
-		SendMes(HelpBuf);
-		if (strcmp(argv[Action], "create")) {
-			n = strtol(argv[NumGame], NULL, 10);
-			sprintf(HelpBuf, ".join %d\n", n);
-			SendMes(HelpBuf);
-			do {
-				GetStr(HelpBuf);
-				if (HelpBuf[0] == '&') {
-					sscanf(HelpBuf, "& %20s", answer);
-				}
-			} while(strcmp(answer, "START"));
-		} else {
-			SendMes(".create\n");
-			n = strtol(argv[NumPlayers], NULL, 10);
-			while(i < n) {
-				GetStr(HelpBuf);
-				if (HelpBuf[0] == '@') {
-					if (HelpBuf[1] == '+') {
-						i++;
-						printf("New player added\n");
-					}
-					if (HelpBuf[1] == '-') {
-						i--;
-						printf("PLayer left the game\n");
-					}
-				}
-			}
-			SendMes("start\n");
-		}
+		struct sockaddr_in addr;
+		if (argc < NumArg)
+			throw "Not enough arguments";	
+		if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+			throw "socket returned -1";
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(strtol(argv[PortNum], NULL, 10));
+		if (!inet_aton(argv[IPAddress], &(addr.sin_addr)))
+			throw "inet_aton returned 0";
+		if (connect(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1)
+			throw "connect returned -1";
 	}
-#endif
+	void GetStr(char *HelpBuf)
+	{
+		char AddBuf[MaxSize];
+		int AddSize, i, n;
+		while((n = SearchEndLine()) == -1) {
+			AddSize = read(fd, AddBuf, MaxSize);
+			if (BufSize + AddSize > MaxSize)
+				throw "Buffer is full";
+			if (AddSize == 0)
+				throw "The connection with server is lost";
+			for(i = 0; i < AddSize; i++)
+				buf[BufSize + i] = AddBuf[i];
+			BufSize += AddSize;
+			buf[BufSize] = 0;
+		}
+		for(i = 0; i < n; i++)
+			HelpBuf[i] = buf[i];
+		HelpBuf[n] = 0;
+		for(i = 0; i < BufSize - n; i++)
+			buf[i] = buf[i + n + 1];
+		BufSize -= n + 1;
+		#ifdef DEBUGGING
+			printf("//%s\n", HelpBuf);
+		#endif 
+	}  
+	void GetOpt(char *CmpOpt, char *HelpBuf)
+	{
+		char option[OptSize] = "";
+		do {
+			GetStr(HelpBuf);
+			if (HelpBuf[0] == '&')
+				sscanf(HelpBuf, "& %20s", option);
+		} while(strcmp(option, CmpOpt));
+	}
+};
 
 class PlayerInfo {
 	int number, status, money, fact, raw, prod, AutoFact;
@@ -234,24 +186,26 @@ public:
 	{
 		return MaxNum;
 	}
-	#ifdef MYSERVER
-		void SetNum(const char *HelpBuf);
-	#else
-		void SetActNum(Socket &sock);
-		void SetNum(Socket &sock, char **argv);
-	#endif
+	void StartGame(Socket &sock, char **argv);
 	void SetInfo(Socket &sock);	
 	void PrintInfo();
+	void SetPlayerInfo(Socket &sock, PlayerInfo *pInfo);
 	void apply(Socket &sock, PlayerInfo *pInfo);
 	void WaitAuction(Socket &sock);
 };
 
 #ifdef MYSERVER
-	void GameInfo::SetNum(const char *HelpBuf)
+	void GameInfo::StartGame(Socket &sock, char **argv)
 	{
+		char HelpBuf[MaxSize];
 		int connect, expect;
+		sock.GetStr(HelpBuf);
+		sock.GetStr(HelpBuf);
 		sscanf(HelpBuf, "#%d%d%d", &MyNum, &connect, &expect);
 		MaxNum = connect + expect;
+		while(sock.CheckStart()) {
+			sock.GetStr(HelpBuf);
+		}
 	}
 	void GameInfo::SetInfo(Socket &sock)
 	{
@@ -279,14 +233,22 @@ public:
 		printf("%18d%10d%18d%10d\n", raw, RawPrice, prod, ProdPrice);
 		printf("--------------------------------------------------------\n");
 	}
+	void GameInfo::SetPlayerInfo(Socket &sock, PlayerInfo *pInfo)
+	{	
+		int i;
+		for(i = 0; i < MaxNum; i++) {
+			(pInfo[i]).SetInfo(sock, i+1);
+			(pInfo[i]).PrintInfo();
+		} 
+	}
 	void GameInfo::apply(Socket &sock, PlayerInfo *pInfo)
 	{
 		char HelpBuf[MaxSize];
-		sprintf(HelpBuf, "prod %d\n", (pInfo[MyNum - 1]).GetFact());
+		sprintf(HelpBuf, "prod %d\n",(pInfo[MyNum-1]).GetFact());
 		sock.SendMes(HelpBuf);
 		(pInfo[MyNum - 1]).SetInfo(sock, MyNum);
 		(pInfo[MyNum - 1]).PrintInfo();
-		sprintf(HelpBuf, "sell %d %d\n", (pInfo[MyNum - 1]).GetProd(), ProdPrice);
+		sprintf(HelpBuf, "sell %d %d\n",(pInfo[MyNum-1]).GetProd(),ProdPrice);
 		sock.SendMes(HelpBuf);
 		sprintf(HelpBuf, "buy %d %d\n", BuyRaw, RawPrice);
 		sock.SendMes(HelpBuf);
@@ -301,52 +263,70 @@ public:
 				printf("%s\n", HelpBuf);
 			}
 		} while(HelpBuf[0] != 'N');
-		printf("%s\n", HelpBuf);
 	}
 #else
-	void GameInfo::SetActNum(Socket &sock)
+	void GameInfo::StartGame(Socket &sock, char **argv)
 	{
 		char HelpBuf[MaxSize];
-		do {
-			sock.GetStr(HelpBuf);
-		} while(HelpBuf[0] != '&');
+		int i = 0, n = strtol(argv[NumPlayersOrNumGame], NULL, 10) ;
+		sprintf(HelpBuf, "%s\n", argv[RobotName]);
+		sock.SendMes(HelpBuf);
+		if (strcmp(argv[Action], "create")) {
+			sprintf(HelpBuf, ".join %d\n", n);
+			sock.SendMes(HelpBuf);
+			sock.GetOpt("START", HelpBuf);
+		} else {
+			sock.SendMes(".create\n");
+			while(i < n) {
+				sock.GetStr(HelpBuf);
+				if (HelpBuf[0] == '@' && HelpBuf[1] == '+') {
+					printf("New player added\n");
+					i++;
+				}
+				if (HelpBuf[0] == '@' && HelpBuf[1] == '-') {
+					printf("PLayer left the game\n");
+					i--;
+				}
+			}
+			sock.SendMes("start\n");
+		}
+		sock.SendMes("info\n");
+		sock.GetOpt("PLAYERS", HelpBuf);
 		sscanf(HelpBuf, "& PLAYERS %d WATCHERS %d", &MaxNum, &watch);
 		ActNum = MaxNum;
-		sock.GetStr(HelpBuf);
-	}
-
-	void GameInfo::SetNum(Socket &sock, char **argv)
-	{
-		int i = 0;
-		char HelpBuf[MaxSize];
-		sock.SendMes("info\n");
-		while (i < 2) {
-			sock.GetStr(HelpBuf);
-			if (HelpBuf[0] == '#') 
-				i++;
-		}
-		SetActNum(sock);
 		MyName = argv[RobotName];
 	}
 	void GameInfo::SetInfo(Socket &sock)
 	{
 		char HelpBuf[MaxSize];
 		sock.SendMes("market\n");
-		do {
-			sock.GetStr(HelpBuf);
-		} while(HelpBuf[0] != '&');
+		sock.GetOpt("MARKET", HelpBuf);
 		sscanf(HelpBuf,"& MARKET %d%d%d%d",&raw,&RawPrice,&prod,&ProdPrice);
-		sock.GetStr(HelpBuf);
 	}
 	void GameInfo::PrintInfo()
 	{
 		printf("--------------------------------------------------------\n");
-		printf("        My Name   Number of Players       Active Players\n");
-		printf("%15s%20d%21d\n", MyName, MaxNum, ActNum);
-		printf("--------------------------------------------------------\n");
 		printf(" Bank sells: items min.price  Bank buys: items max.price\n");
 		printf("%18d%10d%18d%10d\n", raw, RawPrice, prod, ProdPrice);
 		printf("--------------------------------------------------------\n");
+	}
+	void GameInfo::SetPlayerInfo(Socket &sock, PlayerInfo *pInfo)
+	{
+		char HelpBuf[MaxSize], option[OptSize] = "";
+		int i = 0;
+		sock.SendMes("info\n");
+		do {
+			sock.GetStr(HelpBuf);
+			if (HelpBuf[0] == '&') {
+				sscanf(HelpBuf, "& %20s", option);
+				if (!strcmp(option, "INFO")) {
+					pInfo[i].SetInfo(HelpBuf, i);
+					(pInfo[i]).PrintInfo();
+					i++;
+				}
+			}
+		} while(strcmp(option, "PLAYERS"));
+		ActNum = i;
 	}
 	void GameInfo::apply(Socket &sock, PlayerInfo *pInfo)
 	{
@@ -357,74 +337,32 @@ public:
 				break;
 			}
 		}
-		sprintf(HelpBuf, "prod %d\n", (pInfo[MyNum - 1]).GetFact());
+		sprintf(HelpBuf, "prod %d\n",(pInfo[MyNum-1]).GetFact());
 		sock.SendMes(HelpBuf);
-		sprintf(HelpBuf, "sell %d %d\n", (pInfo[MyNum - 1]).GetProd(), ProdPrice);
+		sprintf(HelpBuf, "sell %d %d\n",(pInfo[MyNum-1]).GetProd(),ProdPrice);
 		sock.SendMes(HelpBuf);
-		sprintf(HelpBuf, "buy %d %d\n", BuyRaw, RawPrice);
+		sprintf(HelpBuf, "buy %d %d\n",BuyRaw,RawPrice);
 		sock.SendMes(HelpBuf);
 		sock.SendMes("turn\n");
 	}
 	void GameInfo::WaitAuction(Socket &sock)
 	{
-		char HelpBuf[MaxSize];
-		char answer[AnsSize] = "";
+		char HelpBuf[MaxSize], name[NameSize], option[OptSize] = "";
+		int n, p;
 		do {
 			sock.GetStr(HelpBuf);
 			if (HelpBuf[0] == '&') {
-				sscanf(HelpBuf, "& %20s", answer);
+				sscanf(HelpBuf, "& %20s", option);
+				if (!strcmp(option, "BOUGHT")) {
+					sscanf(HelpBuf, "& BOUGHT %20s%d%d", name, &n, &p);
+					printf("%s bought %d raw by price %d\n", name, n, p);
+				}
+				if (!strcmp(option, "SOLD")) {
+					sscanf(HelpBuf, "& SOLD %20s%d%d", name, &n, &p);
+					printf("%s sold %d products by price %d\n", name, n, p);
+				}
 			}
-		} while(strcmp(answer, "ENDTURN"));
-	}
-#endif
-
-#ifdef MYSERVER
-	void MakeTurn(Socket &sock, GameInfo &gInfo, PlayerInfo *pInfo)
-	{	
-		int i;
-		gInfo.SetInfo(sock);
-		gInfo.PrintInfo();
-		for(i = 0; i < gInfo.GetNum(); i++) {
-			(pInfo[i]).SetInfo(sock, i+1);
-			(pInfo[i]).PrintInfo();
-		} 
-		gInfo.apply(sock, pInfo);
-		gInfo.WaitAuction(sock);
-	}
-	void WaitStart(Socket &sock, GameInfo &gInfo)
-	{
-		char HelpBuf[MaxSize];
-		sock.GetStr(HelpBuf);
-		sock.GetStr(HelpBuf);
-		gInfo.SetNum(HelpBuf);
-		while(sock.CheckStart()) {
-			sock.GetStr(HelpBuf);
-		}
-	}
-#else
-	void MakeTurn(Socket &sock, GameInfo &gInfo, PlayerInfo *pInfo)
-	{
-		int i = 0, j = 0;
-		char HelpBuf[MaxSize];
-		gInfo.SetInfo(sock);
-		sock.SendMes("info\n");
-		do {
-			sock.GetStr(HelpBuf);
-			if (HelpBuf[0] == '#') {
-				j++;
-			}
-			if (HelpBuf[0] == '&') {
-				pInfo[i].SetInfo(HelpBuf, i);
-				i++;
-			}
-		} while(j < 2);
-		gInfo.SetActNum(sock);
-		gInfo.PrintInfo();
-		for(i = 0; i < gInfo.GetNum(); i++) {
-			(pInfo[i]).PrintInfo();
-		}
-		gInfo.apply(sock, pInfo);
-		gInfo.WaitAuction(sock);
+		} while(strcmp(option, "ENDTURN"));
 	}
 #endif
 
@@ -436,15 +374,14 @@ int main(int argc, char **argv)
 	try 
 	{
 		sock.init(argc, argv);
-		#ifdef MYSERVER
-			WaitStart(sock, gInfo);
-		#else 
-			sock.StartGame(argv);
-			gInfo.SetNum(sock, argv);
-		#endif
+		gInfo.StartGame(sock, argv);
 		pInfo = new PlayerInfo [gInfo.GetNum()];
 		for(;;) {
-			MakeTurn(sock, gInfo, pInfo);
+			gInfo.SetInfo(sock);
+			gInfo.PrintInfo();
+			gInfo.SetPlayerInfo(sock, pInfo);
+			gInfo.apply(sock, pInfo);
+			gInfo.WaitAuction(sock);
 		}
 	}
 	catch(const char *s) 
